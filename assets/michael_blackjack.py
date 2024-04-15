2d2959be094e380bf562f93af5a1965bab404ad0
Michael DiPasquale <michaeldipasquale313@gmail.com>
2024-04-15 12:36:02 -0400

    temp removed api_key env file

--- assets/michael_blackjack.py
+++ assets/michael_blackjack.py
@@ -11,9 +11,10 @@ from instructions import header, instructions
 from config import SUITS, RANKS, VALUES
 from database import get_db_engine, init_db, Player, GameSession
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
+from api_key import api_key
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 
 console = Console()

71e5a0999d8a69385e1470d0f100af8fc635d1af
Keenan Weise <kcweise@gmail.com>
2024-04-15 16:31:01 -0500

    Fixed console.print statement in table_bets

--- assets/betting.py
+++ assets/betting.py
@@ -13,12 +13,12 @@ def place_bets(session, max_bet, player_id,
             if bet < 1 or bet > max_bet:
                 print(f"Please enter a valid bet amount from $1 to ${max_bet}.")
                 continue
-            current_money_bag = function_one(player_id)
+            current_money_bag = function_one(session, player_id)
             if bet > current_money_bag:
                 print(f'You cannot bet more than your current funds: {current_money_bag}. Please try again')
                 continue
             new_bag = current_money_bag - bet
-            function_two(player_id, new_bag)
+            function_two(session, player_id, new_bag)
             return bet
         except ValueError:
             print("Invalid input. Please enter a numerical value.")
@@ -28,50 +28,38 @@ def table_bets(session, player_id, money_bag, get_player_money_bag, update_playe
     if 1 <= money_bag <=250:
         max_bet = 20
         current_money_bag = get_player_money_bag(player_id)
-        console.print(f"You currently have ${money_bag}.")
-        user_input = prompt(f"Place a bet less than or equal to ${max_bet}.").strip()
-        new_amount = current_money_bag - user_input
-        update_player_money_bag(session, player_id, new_amount)
+        console.print(f"You currently have ${current_money_bag}.")
+        place_bets(session, max_bet, player_id, get_player_money_bag, update_player_money_bag)
     
     elif 251 <= money_bag <=500:
         max_bet = 50
         current_money_bag = get_player_money_bag(player_id)
-        console.print(f"You currently have ${money_bag}.")
-        user_input = prompt(f"Place a bet less than or equal to ${max_bet}.").strip()
-        new_amount = current_money_bag - user_input
-        update_player_money_bag(session, player_id, new_amount)
+        console.print(f"You currently have ${current_money_bag}.")
+        place_bets(session, max_bet, player_id, get_player_money_bag, update_player_money_bag)
         
     elif 501 <= money_bag <=1000:
         max_bet = 100
         current_money_bag = get_player_money_bag(player_id)
-        console.print(f"You currently have ${money_bag}.")
-        user_input = prompt(f"Place a bet less than or equal to ${max_bet}.").strip()
-        new_amount = current_money_bag - user_input
-        update_player_money_bag(session, player_id, new_amount)
+        console.print(f"You currently have ${current_money_bag}.")
+        place_bets(session, max_bet, player_id, get_player_money_bag, update_player_money_bag)
         
     elif 1001 <= money_bag <=5000:
         max_bet = 250
         current_money_bag = get_player_money_bag(player_id)
-        console.print(f"You currently have ${money_bag}.")
-        user_input = prompt(f"Place a bet less than or equal to ${max_bet}.").strip()
-        new_amount = current_money_bag - user_input
-        update_player_money_bag(session, player_id, new_amount)
+        console.print(f"You currently have ${current_money_bag}.")
+        place_bets(session, max_bet, player_id, get_player_money_bag, update_player_money_bag)
         
     elif 5001 <= money_bag <=10000:
         max_bet = 500
         current_money_bag = get_player_money_bag(player_id)
-        console.print(f"You currently have ${money_bag}.")
-        user_input = prompt(f"Place a bet less than or equal to ${max_bet}.").strip()
-        new_amount = current_money_bag - user_input
-        update_player_money_bag(session, player_id, new_amount)
+        console.print(f"You currently have ${current_money_bag}.")
+        place_bets(session, max_bet, player_id, get_player_money_bag, update_player_money_bag)
         
     elif 10001 < money_bag:
         max_bet = 1000
         current_money_bag = get_player_money_bag(player_id)
-        console.print(f"You currently have ${money_bag}.")
-        user_input = prompt(f"Place a bet less than or equal to ${max_bet}.").strip()
-        new_amount = current_money_bag - user_input
-        update_player_money_bag(session, player_id, new_amount)
+        console.print(f"You currently have ${current_money_bag}.")
+        place_bets(session, max_bet, player_id, get_player_money_bag, update_player_money_bag)
         
         
         
